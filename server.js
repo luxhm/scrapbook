6263868f2e0806d09bd379a34f2ed6363d606bfb
luxhm <lux.hogan-murphy22@trinityschoolnyc.org>
2022-04-06 14:30:39 -0400

    Index

---
+++ server.js
@@ -0,0 +1,25 @@
+//..............Include Express..................................//
+const express = require('express');
+const ejs = require('ejs');
+
+//..............Create an Express server object..................//
+const app = express();
+
+//..............Apply Express middleware to the server object....//
+app.use(express.json()); //Used to parse JSON bodies (needed for POST requests)
+app.use(express.urlencoded());
+app.use(express.static('public')); //specify location of static assests
+app.set('views', __dirname + '/views'); //specify location of templates
+app.set('view engine', 'ejs'); //specify templating library
+
+router.get('/', function(request, response) {
+  response.status(200);
+  response.setHeader('Content-Type', 'text/html')
+  response.render("index");
+});
+
+//..............Start the server...............................//
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('Server started at http://localhost:'+port+'.')
+});
