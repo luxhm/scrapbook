6d9a8106245ab5e4acc5cd47280a85b8d9afd718
luxhm <lux.hogan-murphy22@trinityschoolnyc.org>
2022-04-12 13:01:58 -0400

    working on the sidebar

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.set('view engine', 'ejs'); //specify templating library
 app.get('/', function(request, response) {
   response.status(200);
   response.setHeader('Content-Type', 'text/html')
-  response.render("index");
+  response.render("views/index");
 });
 
 //..............Start the server...............................//
