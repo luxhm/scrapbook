bbc66d5a2f15926da828e9815a1f08974e935fa5
luxhm <lux.hogan-murphy22@trinityschoolnyc.org>
2022-04-29 11:00:46 -0400

    working on socket

--- controllers/edit_controller.js
+++ controllers/edit_controller.js
@@ -4,9 +4,17 @@ router = express.Router();
 const User = require('../models/user_model');
 
 router.get('/edit', function(request, response) {
-  response.status(200);
-  response.setHeader('Content-Type', 'text/html')
-  response.render("views/edit");
+  if(request.user){
+    let user = User.getUser(request.user._json.email);
+    console.log("new attempt");
+    response.status(200);
+    response.setHeader('Content-Type', 'text/html')
+    response.render("views/edit", {
+      userFirstName: user[request.user._json.email].Name
+    });
+  }else{
+    response.redirect('/');
+  }
 });
 
 module.exports = router
--- server.js
+++ server.js
@@ -23,21 +23,16 @@ app.use(require('./controllers/user_controller'));
 app.use(require('./controllers/edit_controller'));
 app.use(require('./controllers/scrapbook_controller'));
 
-let socketapi =require('./controllers/socket_controller');
-socketapi.io.attach(server);//attach sockets to the server
-/*
-app.get('/', function(request, response) {
-  response.status(200);
-  response.setHeader('Content-Type', 'text/html')
-  response.render("views/index");
-});*/
-
 app.use("", function(request, response) {
   response.redirect('/error?code=400');
 });
 
+let socketapi =require('./controllers/socket_controller');
+socketapi.io.attach(server);//attach sockets to the server
+
 //..............Start the server...............................//
 const port = process.env.PORT || 3000;
+app.set('port', port); //let heroku pick the port if needed
 server.listen(port, function() {
   console.log('Server started at http://localhost:'+port+'.')
 });
