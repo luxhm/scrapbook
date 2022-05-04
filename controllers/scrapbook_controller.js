abb1df1c7649782a36d9dc5370e655f418118511
luxhm <lux.hogan-murphy22@trinityschoolnyc.org>
2022-05-04 12:29:46 -0400

    uploading to google drive

--- controllers/scrapbook_controller.js
+++ controllers/scrapbook_controller.js
@@ -7,19 +7,21 @@ router = express.Router();
 const Scrapbook = require('../models/scrapbook_model');
 
 // SET STORAGE
-//defines the destination for private
+let File = require('../models/file_model')
+
+// SET STORAGE
 let privateStorage = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination: function (request, file, cb) {
     cb(null, './uploads')
   },
-  filename: function (req, file, cb) {
-    console.log(file)
+  filename: function (request, file, cb) {
     cb(null, Date.now()+'-'+file.originalname.replace(' ', '-'));
-    //renames the file to make it more specific
   }
 });
 let privateUpload = multer({ storage: privateStorage });
 
+
+/*
 let publicStorage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, './public/images')
@@ -30,9 +32,10 @@ let publicStorage = multer.diskStorage({
   }
 });
 let publicUpload = multer({ storage: publicStorage });
+*/
 
 //Uploading to a public static folder
-router.post('/upload/photo', publicUpload.single('picture'), (req, res, next) => {
+/*router.post('/upload/photo', publicUpload.single('picture'), (req, res, next) => {
   const file = req.file;
   if (!file) {
     const error = {
@@ -44,7 +47,49 @@ router.post('/upload/photo', publicUpload.single('picture'), (req, res, next) =>
   res.render('views/confirmation',{
     photoLocation: "/images/"+file.filename
   });
-})
+})*/
 
+router.post('/upload/photo', privateUpload.single('picture'), async(request, response) => {
+  console.log("/upload/photo route");
+  //const file = request.files[0];
+  const file = request.file;
+  console.log(file);
+
+  if (!file) {
+    const error = {
+    'httpStatusCode' : 400,
+    'message':'Please upload a file'
+     }
+    res.send(error);
+  }
+  let photoLocations=[];
+  let fileURL = await File.uploadFile(file);
+  photoLocations.push(fileURL);
+  response.render('confirmation',{
+    photoLocations: photoLocations
+  });
+})
+/*
+app.post('/uploadfile', privateUpload.any(), async (request, response) => {
+  console.log("/uploadfile route");
+  const file = request.files[0];
+  console.log(file);
+  console.log(request.body.message);
+
+  if (!file) {
+    const error = {
+    'httpStatusCode' : 400,
+    'message':'Please upload a file'
+     }
+    response.send(error);
+  }
+  let photoLocations=[];
+  let fileURL = await File.uploadFile(file);
+  photoLocations.push(fileURL);
+  response.render('confirmation',{
+    photoLocations: photoLocations
+  });
+});
+*/
 
 module.exports = router
---
+++ models/file_model.js
@@ -0,0 +1,67 @@
+/*
+  Uploads file to a single Google Drive folder using a service account
+  https://www.daimto.com/upload-image-to-google-drive-with-node-js/
+  Don't forget to give the service account edit access to the folder
+  and enable View access to anyone on the internet
+*/
+const fs = require('fs');
+const {google} = require('googleapis');
+
+// service account key file from Google Cloud console.
+const KEYS = __dirname+"/../config/keys2.json";
+
+// Request full drive access.
+const SCOPES = ['https://www.googleapis.com/auth/drive'];
+// Create a service account initialize with the service account key file and scope needed
+const auth = new google.auth.GoogleAuth({
+    keyFile: KEYS,
+    scopes: SCOPES
+});
+
+//parents may need to change
+const driveService = google.drive({version: 'v3', auth});
+
+let fileMetadata = {
+  'name': Date.now()+'_image.png',
+  'parents':  ['1PdsY8TKspLcgMRIM0hYtaCAWby4cZAhN']//gohde: TrinLabs/Cookbook/images
+};
+
+
+exports.uploadFile = async function(file) {
+  let fileURL = "";
+  let filePath = __dirname+"/../"+file.path;
+
+  let media = {
+       mimeType: 'image/jpeg',
+       body: fs.createReadStream(filePath)
+   };
+   let response = await driveService.files.create({
+    resource: fileMetadata,
+    media: media,
+    fields: 'id'
+   });
+
+   switch(response.status){
+    case 200:
+        fileURL = "https://drive.google.com/uc?export=view&id="+response.data.id;
+        console.log(fileURL)
+        console.log('Created File Id: ', response.data.id);
+        break;
+    default:
+        console.error('Error creating the file, ' + response.errors);
+        break;
+  }
+
+  return fileURL;
+}
+
+exports.uploadFiles = async function(files) {
+  let fileURLs = [];
+
+  for(let i=0; i<files.length; i++){
+   let fileURL = await exports.uploadFile(files[i]);
+   fileURLs.push(fileURL);
+  }
+
+  return fileURLs;
+}
