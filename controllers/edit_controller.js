82483ef087134340243b41be520effc462ed1b7c
nathalieperra22 <nathalie.perra22@trinityschoolnyc.org>
2022-05-09 20:19:35 -0400

    Working on save button

--- controllers/edit_controller.js
+++ controllers/edit_controller.js
@@ -19,5 +19,28 @@ router.get('/edit', function(request, response) {
   }
 });
 
+router.get('/comments', function(request, response) {
+  response.status(200);
+  response.setHeader('Content-Type', 'text/html')
+  response.render("views/comments");
+});
+
+router.post('/saveScrapbook', function(request, response) {
+  let scrapbookName = request.body.scrapbookName;
+  let pageNumber = request.body.pageNumber;
+  let backgroundColor = request.body.backgroundColor;
+  let images = request.body.images;
+  let fontColor = request.body.fontColor;
+  let font = request.body.font;
+
+  let userID = request.user._json.email;
+
+  Scrapbook.saveScrapbook(scrapbookName, pageNumber, backgroundColor, images, fontColor, font); // scrapbookName, pageNumber, backgroundColor, images, fontColor, font
+
+  response.status(200);
+  response.setHeader('Content-Type', 'text/html');
+  response.redirect("edit");
+});
+
 
 module.exports = router
